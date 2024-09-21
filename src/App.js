90d06672b49903cc9efb08cd4f50678c8a3752c9
Viswanath R <viswa20.2001@gmail.com>
2024-09-21 17:57:31 +0530

    Created Navbar and Home for the Blog webpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>APP COMPONENT</h1>
+        <Home />
       </div>
     </div>
   );
